9024049e316abf9c912498b56f12347f04bb43af
Phan Ngoc Lam <lampn@ames.edu.vn>
2020-09-23 17:33:28 +0700

    fix(example): update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import Xprinter from 'react-native-xprinter';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    Xprinter.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Hello world</Text>
     </View>
   );
 }
