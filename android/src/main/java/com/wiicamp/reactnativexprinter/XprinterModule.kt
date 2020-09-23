2b1ed5d9c7acce751a4e80b0052cb1fdecb0333c
Phan Ngoc Lam <lampn@ames.edu.vn>
2020-09-23 17:21:28 +0700

    feat(android): add print method for android

--- android/src/main/java/com/wiicamp/reactnativexprinter/XprinterModule.kt
+++ android/src/main/java/com/wiicamp/reactnativexprinter/XprinterModule.kt
@@ -1,24 +1,119 @@
 package com.wiicamp.reactnativexprinter
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import android.util.Log
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import net.posprinter.posprinterface.IMyBinder
+import net.posprinter.posprinterface.UiExecute
+import net.posprinter.utils.DataForSendToPrinterPos58
+import java.io.UnsupportedEncodingException
+import java.net.URL
+
 
 class XprinterModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+  //IMyBinder interface，All methods that can be invoked to connect and send data are encapsulated within this interface
+  lateinit var binder: IMyBinder
+  var ISCONNECT = false
+  val DISCONNECT = "com.posconsend.net.disconnetct"
 
-    override fun getName(): String {
-        return "Xprinter"
+  //bindService connection
+  val conn: ServiceConnection = object : ServiceConnection {
+    override fun onServiceConnected(componentName: ComponentName, iBinder: IBinder) {
+      //Bind successfully
+      binder = iBinder as IMyBinder
+
+      binder.connectNetPort("127.0.0.1", 9100, object : UiExecute {
+        override fun onsucess() {
+          ISCONNECT = true
+
+          binder.acceptdatafromprinter(object : UiExecute {
+            override fun onsucess() {
+
+            }
+
+            override fun onfailed() {
+              ISCONNECT = false;
+              val intent = Intent();
+              intent.action = DISCONNECT;
+              reactContext.sendBroadcast(intent);
+            }
+          });
+        }
+
+        override fun onfailed() {
+          ISCONNECT = false
+        }
+
+      })
+      Log.e("connect binder", "connected")
     }
 
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
+    override fun onServiceDisconnected(componentName: ComponentName) {
+      Log.e("disconnect binder", "disconnected")
     }
+  }
 
-    
+  override fun getName(): String {
+    return "Xprinter"
+  }
+
+  override fun initialize() {
+    super.initialize()
+
+
+  }
+
+  @ReactMethod
+  fun connectViaNetwork(ipAddress: String, promise: Promise) {
+    if (ipAddress.isNullOrEmpty()) {
+      promise.reject("INVALID_IP_ADDRESS", "Invalid ip address. Please call connectViaNetwork with valid ip address.");
+      return;
+    }
+  }
+
+  @ReactMethod
+  fun print(text: String) {
+//    val bytes = URL(filePath).readBytes()
+    val list = ArrayList<ByteArray>()
+
+    list.add(DataForSendToPrinterPos58.initializePrinter())
+    val data1: ByteArray = strTobytes(text)
+    list.add(data1)
+    //should add the command of print and feed line,because print only when one line is complete, not one line, no print
+    //should add the command of print and feed line,because print only when one line is complete, not one line, no print
+    list.add(DataForSendToPrinterPos58.printAndFeedLine())
+    //cut pager
+    //cut pager
+    list.add(DataForSendToPrinterPos58.selectOrCancelCW90(1))
+
+//    binder.write(DataForSendToPrinterPos58., object: UiExecute {
+//      override fun onsucess() {
+//        TODO("Not yet implemented")
+//      }
+//
+//      override fun onfailed() {
+//        TODO("Not yet implemented")
+//      }
+//
+//    });
+  }
+
+  private fun strTobytes(str: String): ByteArray {
+    var b: ByteArray = ByteArray(0);
+    var data: ByteArray = ByteArray(0);
+    try {
+      b = str.toByteArray(charset("utf-8"))
+      data = String(b, Charsets.UTF_8).toByteArray(charset("gbk"))
+    } catch (e: UnsupportedEncodingException) {
+      // TODO Auto-generated catch block
+      e.printStackTrace()
+    }
+    return data
+  }
 }
